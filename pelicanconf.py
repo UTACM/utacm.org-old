016d8500d39a0ea0933a029a4e5b73d090724d9e
William Ting <io@williamting.com>
2013-09-20 14:43:22 -0500

    fix page linking, relative urls

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,21 +18,6 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-# Blogroll
-LINKS =  (
-        ('UT Computer Science', 'http://www.cs.utexas.edu/'),
-        ('Women in CS', 'http://www.cs.utexas.edu/~wics/'),
-        ('TSSA', 'http://www.turingscholars.org/'),
-        ('ELA', 'http://www.cs.utexas.edu/users/ela/'),
-        ('UT Programming Club', 'http://userweb.cs.utexas.edu/users/utpc/'),
-        )
-
-# Social widget
-SOCIAL = (
-        ('Facebook Group', 'https://www.facebook.com/groups/utexas.acm/'),
-        ('Events Calendar', 'https://www.google.com/calendar/render?cid=2v4vtrfjc0k2dc3s935gulfr70%40group.calendar.google.com'),
-        )
-
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
