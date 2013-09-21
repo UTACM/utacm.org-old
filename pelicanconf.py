c3d80aca2fda93998a55678d231ddedc1140eed1
William Ting <io@williamting.com>
2013-09-20 23:02:05 -0500

    really fix broken links on sub-pages

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,6 +6,7 @@ AUTHOR = u'William Ting'
 SITENAME = u'Association for Computing Machinery'
 TAGLINE = u'University of Texas Chapter'
 SITEURL = ''
+RELATIVE_URLS = True
 
 TIMEZONE = 'America/Chicago'
 
@@ -13,19 +14,15 @@ DEFAULT_CATEGORY = u'general'
 DEFAULT_LANG = u'en'
 DISPLAY_PAGES_ON_MENU = True
 DISPLAY_CATEGORIES_ON_MENU = False
-
-MENUITEMS = [('Home', SITEURL)]
+DEFAULT_PAGINATION = False
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-DEFAULT_PAGINATION = False
-
-# Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
-
 STATIC_PATHS = ['images', 'pdfs']
-THEME = 'theme'
 FILES_TO_COPY = [('images/favicon.ico', 'favicon.ico')]
+
+THEME = 'theme'
+MENUITEMS = [('Home', '/')]
